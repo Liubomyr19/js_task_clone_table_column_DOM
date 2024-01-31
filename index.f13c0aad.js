3fcd87f75c38fe382a992c55791d2d847674756f
Lubomur Chuchvara <lubomircucvara@gmail.com>
2024-01-31 12:06:30 +0200

    make build

---
+++ index.f13c0aad.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e,n=document.querySelector("table"),r=(e=[],n.querySelectorAll("tr").forEach(function(n){var r=n.children[1].cloneNode(!0);e.push(r)}),e);!function(e,n,r){e.querySelectorAll("tr").forEach(function(e,t){var o=e.children;r<o.length?e.insertBefore(n[t],o[r]):e.appendChild(n[t])})}(n,r,4)});
+//# sourceMappingURL=index.f13c0aad.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,27 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    const table = document.querySelector("table");
+    const secondCoolumn = cloneColumn(table, 2);
+    insertColumn(table, secondCoolumn, 4);
+});
+function cloneColumn(table, columnIndex) {
+    const clonedColumn = [];
+    const rows = table.querySelectorAll("tr");
+    rows.forEach((row)=>{
+        const cells = row.children;
+        const originalCell = cells[columnIndex - 1];
+        const clonedCell = originalCell.cloneNode(true);
+        clonedColumn.push(clonedCell);
+    });
+    return clonedColumn;
+}
+function insertColumn(table, column, index) {
+    const rows = table.querySelectorAll("tr");
+    rows.forEach((row, rowIndex)=>{
+        const cells = row.children;
+        if (index < cells.length) row.insertBefore(column[rowIndex], cells[index]);
+        else row.appendChild(column[rowIndex]);
+    });
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
